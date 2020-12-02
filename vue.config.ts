65b9e523ed5beb51e5676d3f9e54240f0066f35c
lucka-me <i@lucka.moe>
2020-12-02 15:35:10 +0800

    Get publicPath from config.yaml

--- vue.config.ts
+++ vue.config.ts
@@ -1,14 +1,19 @@
+import fs from 'fs';
 import path from 'path';
 import { ProjectOptions } from '@vue/cli-service';
+import YAML from 'yaml';
 
-const config: ProjectOptions = {
-    publicPath: '/wikist/',
+const configFile = fs.readFileSync('./config.yaml', 'utf-8');
+const config = YAML.parse(configFile);
+
+const options: ProjectOptions = {
+    publicPath: config.project.publicPath || '/wikist/',
     chainWebpack: (config) => {
         config.resolve.extensions.add('yaml');
         config.module.rule('yaml').test(/\.ya?ml?$/).type('json')
             .use('yaml-loader').loader('yaml-loader').end();
         config.resolve.alias.set('@config', path.resolve(__dirname, 'config.yaml'));
-    }
+    },
 };
 
-module.exports = config;
+module.exports = options;
